4d49e4f7b2f0f258565dba1bbc69f5e5374a2165
Goyo-Frank <francoedquid@gmail.com>
2024-12-02 16:30:39 +0800

    Main Code for Yap Daily Dose
    just import the main CS file for this

---
+++ DailyMenu.cs
@@ -0,0 +1,13 @@
+namespace YapDailyDoseCashierSystem;
+
+public class DailyMenu
+{
+    public static void MenuList()
+    {
+        const float cheeseburger = 4.99f;
+        const float grilledChickenSandwich = 6.49f;
+        const float caesarSalad = 5.26f;
+        const float fries = 2.99f;
+        const float chocolateMilkshake = 3.75f; 
+    }
+}
---
+++ YapDailyDose.cs
@@ -0,0 +1,60 @@
+﻿
+
+namespace YapDailyDoseCashierSystem
+{
+    class YapDailyDoseCashierSystem
+    {
+        static void Main()
+        {
+            Console.Title = "Yap Daily Dose Cashier System";
+            Banner();
+            Menu();
+            Console.Write("\nSelect an option: ");
+            
+            
+            
+            Console.ReadKey();
+        }
+
+        static void Menu()
+        {
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Welcome to Daily Dose YAP, How can I help you?");
+
+            string[] menuItems = new[]
+            {
+                "1. Order Cheeseburger",
+                "2. Order Grilled Chicken Sandwich",
+                "3. Order Caesar Salad",
+                "4. Order Fries ",
+                "5. Order Chocolate Milkshake",
+                "6. Print Receipt",
+                "7. Exit Program",
+            };
+
+            foreach (string menuItem in menuItems)
+            {
+                Console.WriteLine(menuItem);
+            }
+            
+            
+
+        }
+
+        static void Banner()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"
+ __   __           ___       _ _        ___             
+ \ \ / __ _ _ __  |   \ __ _(_| |_  _  |   \ ___ ______ 
+  \ V / _` | '_ \ | |) / _` | | | || | | |) / _ (_-/ -_)
+   |_|\__,_| .__/ |___/\__,_|_|_|\_, | |___/\___/__\___|
+           |_|                   |__/                   
+
+                                    made by: @frankieed_
+_______________________________________________________________
+");
+        }
+    }
+}
